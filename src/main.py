a1e4351312025d9098aeb3a9f53d1ff002add037
lt468 <lucatremblayuk@gmail.com>
2022-08-26 16:31:33 +0100

    Just some updates, saving as I go

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print("Hello, github!")
+# Importing libraries
+import pygame
+
+# Initializing pygame
+pygame.init()
+
+# Creating screen of width 800px, height 600px
+gameDisplay = pygame.display.set_mode((800, 600))
+
+# Setting title and icon
+pygame.display.set_caption("algxbra")
+icon = pygame.image.load("../mda/algxbra_icon.png")
+pygame.display.set_icon(icon)
+
+menuScreen = True
+
+while menuScreen:
+    # Checking if quit button pressed
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            menuScreen = False
+    pygame.display.update()
+    pass
+    
+
